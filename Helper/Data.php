dd4a71d1438bab8c7c71e7ed692cc3891a17d78d
Kostadin Bashev <bashev@webcode.bg>
2021-11-08 15:27:27 +0200

    v.0.2.2

--- Helper/Data.php
+++ Helper/Data.php
@@ -118,7 +118,7 @@ class Data extends AbstractHelper
      */
     public function getFeedPath(bool $withFilename = false): string
     {
-        return $this->directoryList->getPath(DirectoryList::PUB) . DS . self::FEED_DIR . DS .
+        return $this->directoryList->getPath(DirectoryList::PUB) . self::DS . self::FEED_DIR . self::DS .
                ($withFilename === true ? $this->getFeedFilename() : '');
     }
 
