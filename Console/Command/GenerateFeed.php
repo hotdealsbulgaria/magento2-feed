7820ab03aaeb0b3a38352e04f471730d67ff26cf
Kostadin Bashev <bashev@webcode.bg>
2021-11-08 17:29:48 +0200

    v.0.2.2

--- Console/Command/GenerateFeed.php
+++ Console/Command/GenerateFeed.php
@@ -75,10 +75,10 @@ class GenerateFeed extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln(__('<info>%1</info>', 'Start Generating HotDeals Feed...'));
+        $output->writeln(sprintf('<info>%1</info>', 'Start Generating HotDeals Feed...'));
         $this->generateFeedService->execute();
         $output->write(PHP_EOL);
-        $output->writeln(__('<info>%1</info>', 'HotDeals Feed was generated.'));
+        $output->writeln(sprintf('<info>%1</info>', 'HotDeals Feed was generated.'));
 
         return Cli::RETURN_SUCCESS;
     }
